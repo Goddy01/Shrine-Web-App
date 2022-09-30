97abbc24f2daed55529e91cbe5fff0fdfb83ad64
Godwin Adigun <adigungodwin2@gmail.com>
2022-09-30 14:13:54 +0100

    added user_reg_view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
 # Create your views here.
+
+def user_reg_view(request):
+    if request.method == 'POST':
+        reg_form = forms.UserRegForm(request.POST)
+        if reg_form.is_valid():
+            reg_form.save()
+            login(request, reg_form)
+            return redirect()
+        else:
+            messages.error(request, 'Registration Failed')
+    else:
+        reg_form = forms.UserRegForm()
+    return render(request,)
