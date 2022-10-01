fa2559d788f7d744172073c88c698ebe377fd0b6
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-01 16:13:33 +0100

    updates

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,10 @@
 from django import forms
-from . import models
+from .models import UserProfile
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegForm(UserCreationForm):
     class Meta:
-        model = models.UserProfile
-        fields = '__all__'
+        model = UserProfile
+        fields = ['first_name', 'last_name', 'username', 'phone_number', 'country', 'email','password1', 'password2']
+        # exclude = ('last_login', 'date_joined', 'is_admin', 'is_active', 'is_superuser', 'is_staff', 'is_worshipper')
+        # fields = '__all__'
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from . import forms
+from .forms import UserRegForm
 
 # Create your views here.
 
 def user_reg_view(request):
     if request.method == 'POST':
-        reg_form = forms.UserRegForm(request.POST)
+        reg_form = UserRegForm(request.POST)
         if reg_form.is_valid():
             reg_form.save()
             login(request, reg_form)
@@ -15,5 +15,5 @@ def user_reg_view(request):
         else:
             messages.error(request, 'Registration Failed')
     else:
-        reg_form = forms.UserRegForm()
+        reg_form = UserRegForm()
     return render(request, 'accounts/register.html', {'reg_form': reg_form})
