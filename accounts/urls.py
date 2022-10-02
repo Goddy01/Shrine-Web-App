af82aaa66afb68b5b41dd08528fb278ec4fd6e67
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-02 20:29:08 +0100

    added logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('sign-up/', views.user_reg_view, name='reg'),
     path('sign-in/', views.user_login_view, name='login'),
+    path('sign-in/', views.logout_view, name='logout'),
 ]
