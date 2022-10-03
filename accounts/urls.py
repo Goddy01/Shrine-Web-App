08725c9cb63af5525bcd7720a709fc32cbe20027
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-03 11:42:33 +0100

    fixed password reset not working

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, reverse_lazy
+from django.urls import path, reverse_lazy, include
 from django.contrib.auth import views as auth_views
 from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('sign-up/', views.user_reg_view, name='reg'),
     path('sign-in/', views.user_login_view, name='login'),
     path('sign-out/', views.logout_view, name='logout'),
@@ -17,7 +18,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='accounts/password/password_reset_form.html', 
         success_url = reverse_lazy('password_reset_complete'),
-    ), name="password_reset_confirm"),    
+    ), name="password_reset_confirm"),
 
     path('password_reset/', auth_views.PasswordResetView.as_view(
         template_name='accounts/password/password_reset.html',
