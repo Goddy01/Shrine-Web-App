e4657cefb3fafbc841b608cd03fff0236bc78d5d
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-05 00:34:39 +0100

    added event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Event(models.Model):
+    event_id =          models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    event_name =        models.CharField(max_length=256, null=False, blank=False)
+    event_date =        models.CharField(max_length=128, null=False, blank=False)
+    event_time =        models.CharField(max_length=128, null=False, blank=False)
+    event_location =    models.CharField(max_length=128, null=False, blank=False)
+    event_url =         models.URLField(max_length=256)
+    event_description = models.TextField(max_length=10000, blank=False, null=False)
+
+    def __str__(self):
+        return self.event_id
