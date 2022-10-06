4b7f8dcd2a03b2ce7594230ed567526044c8c7ce
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-06 18:44:35 +0100

    added update_event url

--- events/urls.py
+++ events/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add-event/', views.add_event, name='add_event'),
     path('events/', views.event, name='event'),
     path('<event_id>/event_detail/', views.event_detail, name='event_detail'),
+    path('<event_id>/update_event/', views.update_event, name='update_event'),
 ]
