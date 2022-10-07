75e3f3f515fa23be42adf483273ca0da241c807a
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-07 08:51:30 +0100

    added Question model

--- events/models.py
+++ events/models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from accounts.models import UserProfile
 import uuid
 
 # Create your models here.
@@ -14,3 +15,12 @@ class Event(models.Model):
 
     def __str__(self):
         return self.event_id
+
+class Question(models.Model):
+    event =         models.ForeignKey(Event, on_delete=models.CASCADE)
+    user =          models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    question_id =   models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    question =      models.TextField(max_length=5000, blank=False, null=False)
+
+    def __str__(self):
+        return self.question_id
