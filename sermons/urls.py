5c5047f5379e892b421d68434b31171def92ef83
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-11 10:25:08 +0100

    added sermon_details url

--- sermons/urls.py
+++ sermons/urls.py
@@ -5,4 +5,5 @@ app_name = 'sermons'
 urlpatterns = [
     path('sermons/', views.sermons, name='sermons'),
     path('add-sermon/', views.add_sermon, name='add_sermon'),
+    path('<sermon_id>/sermon_details/', views.sermon_details, name='sermon_details'),
 ]
