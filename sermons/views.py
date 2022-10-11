47ac6f4861633ab97b0f056f766490eb49bc711b
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-11 09:04:01 +0100

    refactoring add_sermon

--- sermons/views.py
+++ sermons/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import AddSermonForm, UpdateSermonForm
+from django.contrib import messages
 # Create your views here.
 
 def add_sermon(request):
@@ -8,10 +9,14 @@ def add_sermon(request):
         if request.method == 'POST':
             add_sermon_form = AddSermonForm(request.POST or None, request.FILES or None)
             if add_sermon_form.is_valid():
+                add_sermon_form = add_sermon_form.save(commit=False)
+                if add_sermon_form.is_past_due():
+                    messages.error(request, 'The date of the sermon cannot be in the past.')
                 add_sermon_form.save()
                 return redirect('home')
             else:
                 add_sermon_form = AddSermonForm()
+                messages.error(request, 'The sermon could not be added.')
         else:
             add_sermon_form = AddSermonForm()
     else:
