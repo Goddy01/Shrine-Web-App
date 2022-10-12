d955ddb0e1490a83c43342765085ac215183079e
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-12 11:51:07 +0100

    added delete_sermon url

--- sermons/urls.py
+++ sermons/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add-sermon/', views.add_sermon, name='add_sermon'),
     path('<sermon_id>/sermon_details/', views.sermon_details, name='sermon_details'),
     path('<sermon_id>/update_sermon/', views.update_sermon, name='update_sermon'),
+    path('<sermon_id>/delete_sermon/', views.delete_sermon, name='delete_sermon'),
 ]
