a5ad74f2573ba1c7417aeca2cba9eb09064a01e0
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-12 10:58:26 +0100

    refactored sermons.forms

--- sermons/forms.py
+++ sermons/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from .models import Sermon
 
 class AddSermonForm(forms.ModelForm):
+    sermon_image = forms.ImageField(widget=forms.FileInput, required=True)
     class Meta:
         model = Sermon
         exclude = ('sermon_id', 'slug')
 
 class UpdateSermonForm(forms.ModelForm):
+    sermon_image = forms.ImageField(widget=forms.FileInput, required=False)
     class Meta:
         model = Sermon
         exclude = ('sermon_id', 'slug')
