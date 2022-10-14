c2134173274e03444e7a5784ade344c714ad7d53
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-14 22:53:28 +0100

    added news url

--- news/views.py
+++ news/views.py
@@ -6,11 +6,13 @@ from .models import News
 # Create your views here.
 
 def news(request):
-    news = News.objects.all().order_by('date_posted')
+    """The view for rendering all news on the news page"""
+    news = News.objects.all().order_by('-date_posted')
     latest_news = News.objects.all().order_by('-date_posted')
     return render(request, 'news/news.html', {'news': news, 'latest_news': latest_news})
 
 def add_news(request):
+    """The view that facilitates the adding of news"""
     if request.user.is_staff:
         if request.method == 'POST':
             add_news_form = AddNewsForm(request.POST)
@@ -25,3 +27,8 @@ def add_news(request):
     else:
         return HTTPResponse('Sorry. You are not authorized to post news.')
     return render(request, 'news/add_news.html', {'add_news_form': add_news_form})
+
+def news_details(request, news_id):
+    """The view that facilitates the display of news details"""
+    news = News.objects.get(news_id=news_id)
+    return render(request, 'news/news_details.htl', {'news': news})
