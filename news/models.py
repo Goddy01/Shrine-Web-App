92231fa3a51106e5f1ba4cd81914d61958927757
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-14 23:17:45 +0100

    altered date_posted field in news.models.news

--- news/models.py
+++ news/models.py
@@ -7,7 +7,7 @@ class News(models.Model):
     news_id =           models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
     news_headline =     models.CharField(max_length=256, blank=False, null=False)
     news_body =         RichTextField(max_length=20000, blank=False, null=False)
-    date_posted =       models.DateTimeField(auto_now_add=True, null=True)
+    date_posted =       models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.news_id
