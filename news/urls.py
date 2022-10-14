73a02fbc11abb9b4478b147bfa8dd2a1dbc8a50f
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-14 23:31:33 +0100

    added delete_news url

--- news/urls.py
+++ news/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add-news/', views.add_news, name='add_news'),
     path('<news_id>/news_details/', views.news_details, name='news_details'),
     path('<news_id>/update_news_details/', views.update_news, name='update_news_details'),
+    path('<news_id>/delete_news/', views.delete_news, name='delete_news'),
 ]
