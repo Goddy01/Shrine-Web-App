6cd585175debc23c82c232e22ae5b80ae4c97246
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-15 17:49:51 +0100

    added donate_temp url

--- donations/urls.py
+++ donations/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('donations/', views.donations, name='donations'),
     path('add-donation/', views.add_donation, name='add_donation'),
     path('<donation_id>/donation_details/', views.donation_details, name='donation_details'),
+    path('<donation_id>/donate_temp/', views.donate_temp, name='donate_temp'),
 ]
