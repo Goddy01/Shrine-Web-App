9cc579818e60679c9e9ec0891e285e6d6b2a9a8e
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-15 15:17:21 +0100

    added donations url

--- donations/urls.py
+++ donations/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'donations'
 
 urlpatterns = [
+    path('donations/', views.donations, name='donations'),
     path('add-donation/', views.add_donation, name='add_donation'),
 ]
