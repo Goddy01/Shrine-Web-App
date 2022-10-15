68a7f218b75886122d907dc0ad08f32432f2212d
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-15 15:55:13 +0100

    added content to donations

--- donations/models.py
+++ donations/models.py
@@ -7,7 +7,7 @@ class Donation(models.Model):
     donation_id =           models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
     donation_name =         models.CharField(max_length=256, null=False, blank=False)
     donation_desc =         models.TextField(max_length=5000, blank=False, null=False)
-    amount_needed =       models.IntegerField(null=False, blank=False)
+    amount_needed =         models.IntegerField(null=False, blank=False)
     date_posted =           models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
--- donations/views.py
+++ donations/views.py
@@ -16,7 +16,6 @@ def add_donation(request):
                 return redirect('donations:donations')
             else:
                 add_donation_form = AddDonationForm()
-                print(add_donation_form.errors)
                 messages.error(request, 'The donation could not be added.')
         else:
             add_donation_form = AddDonationForm()
