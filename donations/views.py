84bc8c618ce9b2b85dc5466a1bbc296947d52a5f
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-15 14:42:12 +0100

    added add_donation view

--- donations/views.py
+++ donations/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
+from django.contrib import messages
 from django.shortcuts import render
 
 # Create your views here.
+def add_donation(request):
+    if request.user.is_staff:
+        if request,method == 'POST':
+            add_donation_form = AddDonationForm(request.POST)
+            if add_donation_form.is_valid():
+                add_donation_form.save()
+            else:
+                add_donation_form = AddDonationForm()
+                messages.error(request, 'The donation could not be added.')
+        else:
+            add_donation_form = AddDonationForm()
+    else:
+        return HTTPResponse('Sorry. You are not authenticated to add donations.')
+    return render(request, 'donations/add_donation.html', {'add_donation_form': add_donation_form})
