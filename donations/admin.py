ab59c18c50d7aa9a5ad4a38848b604d304ebb796
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-15 18:12:46 +0100

    reg'd Donated

--- donations/admin.py
+++ donations/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Donation
+from .models import Donation, Donated
 # Register your models here.
 admin.site.register(Donation)
 admin.site.register(Donated)
