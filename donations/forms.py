fc63a9ba6b5924b8ee4ff79f241e1b73304b03f6
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-19 09:00:14 +0100

    added DonateForm

--- donations/forms.py
+++ donations/forms.py
@@ -1,7 +1,12 @@
-from .models import Donation
+from .models import Donation, Donate
 from django import forms
 
 class AddDonationForm(forms.ModelForm):
     class Meta:
         model = Donation
         exclude = ['donation_id', 'date_posted']
+
+class DonateForm(forms.ModelForm):
+    class Meta:
+        model = Donate
+        fields = '__all__'
