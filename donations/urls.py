975d73dba95432fd1ebd562fc4d61284d026ee9b
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-22 21:22:44 +0100

    added delete_donation url

--- donations/urls.py
+++ donations/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<donation_id>/update_donation/', views.update_donation, name='update_donation'),
     path('<donation_id>/donation_details/', views.donation_details, name='donation_details'),
     path('<donation_id>/make_donation/', views.make_payment, name='make_donation'),
+    path('<donation_id>/delete_donation/', views.delete_donation, name='del_donation'),
 ]
