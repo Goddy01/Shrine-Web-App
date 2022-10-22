8d8c27dad281f67e3046f3df21671a54ed459055
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-22 09:12:51 +0100

    added update_donation url

--- donations/urls.py
+++ donations/urls.py
@@ -6,6 +6,7 @@ app_name = 'donations'
 urlpatterns = [
     path('donations/', views.donations, name='donations'),
     path('add-donation/', views.add_donation, name='add_donation'),
+    path('<donation_id>/update_donation/', views.update_donation, name='update_donation'),
     path('<donation_id>/donation_details/', views.donation_details, name='donation_details'),
     path('<donation_id>/make_donation/', views.make_payment, name='make_donation'),
 ]
