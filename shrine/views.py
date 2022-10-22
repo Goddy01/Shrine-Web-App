58e13686c9c7f635c294b49e8f626abb10dc44da
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-22 23:40:10 +0100

    rendered objects on the home temp

--- shrine/views.py
+++ shrine/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from news.models import News
 from events.models import Event
 from donations.models import Donation
+from sermons.models import Sermon
 # Create your views here.
 def home(request):
     news = News.objects.all().order_by('-date_posted')[:3]
-    events = Event.objects.all().order_by('event_date')[:3]
+    events = Event.objects.all().order_by('-event_date')[:3]
     donations = Donation.objects.filter(complete=False)[:3]
     sermons = Sermon.objects.all().order_by('-sermon_date')[:3]
     return render(request, 'home.html', {'news': news, 'events': events, 'donations': donations, 'sermons': sermons})
