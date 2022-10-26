f118d60b8db8722b464d57486d0fb6ea1914c43b
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-26 16:04:31 +0100

    refactored context data in home view

--- shrine/views.py
+++ shrine/views.py
@@ -4,15 +4,16 @@ from events.models import Event
 from donations.models import Donation
 from sermons.models import Sermon
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from datetime import date
 
 
 # Create your views here.
 def home(request):
     """The view for rendering info on the home page"""
     news = News.objects.all().order_by('-date_posted')[:3]
-    events = Event.objects.all().order_by('-event_date')[:3]
+    events = Event.objects.filter(event_date__gte=date.today()).order_by('event_date')[:3]
     donations = Donation.objects.filter(complete=False)[:3]
-    sermons = Sermon.objects.all().order_by('-sermon_date')[:3]
+    sermons = Sermon.objects.filter(sermon_date__gte=date.today()).order_by('sermon_date')[:3]
     return render(request, 'home.html', {'news': news, 'events': events, 'donations': donations, 'sermons': sermons})
 
 def pagination(request, items_list, num_of_pages, object_id):
