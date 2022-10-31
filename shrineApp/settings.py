86d319779af7d0d9a455e3a437e834b6573d9cec
Godwin Adigun <adigungodwin2@gmail.com>
2022-10-31 23:07:22 +0100

    fixing bugs

--- shrineApp/settings.py
+++ shrineApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ob6$p=ts1wc46jqy&w#zt(vkbozdk1752==6t$i1^xc7!j%j(_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
