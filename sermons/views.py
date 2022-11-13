1aa93378c7cedf98ecdde5b86273b1448a5897f9
Godwin Adigun <adigungodwin2@gmail.com>
2022-11-13 14:30:02 +0100

    fixed empty sequence bug in sermons view

--- sermons/views.py
+++ sermons/views.py
@@ -11,16 +11,22 @@ from shrine.views import pagination
 
 def sermons(request):
     """The view for rendering all the sermons on the sermon page"""
+    context = {}
     sermons = Sermon.objects.all().order_by('sermon_date')
     past_sermons = Sermon.objects.filter(sermon_date__lte=date.today()).order_by('-sermon_date')[:2]
     upcoming_sermons = Sermon.objects.filter(sermon_date__gte=date.today())
     donations = Donation.objects.filter(complete=False)
     donations = list(donations)
-    donation = random.choice(donations)
-    
+    if donations:
+        donation = random.choice(donations)
+        context['donation'] = donation
     # Using the Pagination template from shrine.views
     sermons_pag = pagination(request, upcoming_sermons, 3, 'sermon_date')
-    return render(request, 'sermons/sermons.html', {'sermons':sermons, 'past_sermons': past_sermons, 'donation': donation, 'upcoming_sermons': upcoming_sermons, 'sermons_pag': sermons_pag})
+    context['sermons'] = sermons
+    context['past_sermons'] = past_sermons
+    context['upcoming_sermons'] = upcoming_sermons
+    context['sermons_pag'] = sermons_pag
+    return render(request, 'sermons/sermons.html', context)
 
 def sermon_error_checker(request,sermon_form):
     """The view that checks for errors in sermon forms. Will be called when needed to abide by DRY"""
