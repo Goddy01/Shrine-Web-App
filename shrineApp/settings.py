83e86316e86bf4983676f3cf6b25148ad20ed5af
Toshihiro <adigungodwin2@gmail.com>
2022-11-26 14:33:20 +0100

    env

--- shrineApp/settings.py
+++ shrineApp/settings.py
@@ -204,8 +204,8 @@ CKEDITOR_CONFIGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bloggie.blogapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'pktwaipegwuzmigr'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Oduduwa Shrine Team <noreply@oduduwa.com>'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
